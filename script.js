f3e78901408dc62450f951fde318b40fac9cdbe0
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 21:34:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,72 +1,23 @@
-//const inputs = document.querySelectorAll('.controls input');
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
 
-  //  function handleUpdate() {
-    //  const suffix = this.dataset.sizing || '';
-      //document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    //}
+function handleMove(e) {
+  const y = e.offsetY;
+  const height = this.offsetHeight;
 
-    //inputs.forEach(input => input.addEventListener('change', handleUpdate));
-   // inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
-const video = document.querySelector('.player__video');
-const toggle = document.querySelector('.toggle');
-const rewind = document.querySelector('.rewind');
-const forward = document.querySelector('.forward');
-const volume = document.querySelector('input[name="volume"]');
-const playbackRate = document.querySelector('input[name="playbackRate"]');
-const progressFilled = document.querySelector('.progress__filled');
+  const min = 0.4;
+  const max = 4;
 
-/* ---------- PLAY / PAUSE ---------- */
-function togglePlay() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  const percent = y / height;
+  const playbackRate = percent * (max - min) + min;
+
+  bar.style.height = `${Math.round(percent * 100)}%`;
+  bar.textContent = `${playbackRate.toFixed(2)}×`;
+  video.playbackRate = playbackRate;
 }
 
-function updateButton() {
-  toggle.textContent = video.paused ? '►' : '❚❚';
-}
+speed.addEventListener('mousemove', handleMove);
 
-/* ---------- PROGRESS ---------- */
-function handleProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressFilled.style.flexBasis = `${percent}%`;
-}
-
-/* ---------- SKIP ---------- */
-function rewindVideo() {
-  video.currentTime -= 10;
-}
-
-function forwardVideo() {
-  video.currentTime += 25;
-}
-
-/* ---------- RANGE CONTROLS ---------- */
-function handleRangeUpdate() {
-  video[this.name] = this.value;
-}
-
-/* ---------- EVENT LISTENERS ---------- */
-if (video && toggle) {
-  video.addEventListener('click', togglePlay);
-  video.addEventListener('play', updateButton);
-  video.addEventListener('pause', updateButton);
-  video.addEventListener('timeupdate', handleProgress);
-
-  toggle.addEventListener('click', togglePlay);
-}
-
-if (rewind) rewind.addEventListener('click', rewindVideo);
-if (forward) forward.addEventListener('click', forwardVideo);
-
-if (volume) {
-  volume.addEventListener('change', handleRangeUpdate);
-  volume.addEventListener('mousemove', handleRangeUpdate);
-}
-
-if (playbackRate) {
-  playbackRate.addEventListener('change', handleRangeUpdate);
-  playbackRate.addEventListener('mousemove', handleRangeUpdate);
-}
+    inputs.forEach(input => input.addEventListener('change', handleUpdate));
+   inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
